c7712868654066b9651c8f2508b0e2ad53d0696d
Prathamesh Patel <prathamesh.patel15@gmail.com>
2023-05-22 01:14:48 +0530

    migrated to Manifest V3

--- background.js
+++ background.js
@@ -1,19 +1,36 @@
 const defaultFilters = [
-	"*://*.doubleclick.net/*",
-	"*://partner.googleadservices.com/*",
-	"*://*.googlesyndication.com/*",
-	"*://*.google-analytics.com/*",
-	"*://creative.ak.fbcdn.net/*",
-	"*://*.adbrite.com/*",
-	"*://*.exponential.com/*",
-	"*://*.quantserve.com/*",
-	"*://*.scorecardresearch.com/*",
-	"*://*.zedo.com/*",
-]
-
-chrome.webRequest.onBeforeRequest.addListener(
-    function(details) { return { cancel: true }},
-    { urls: defaultFilters },
-    ["blocking"]
-)
-
+    "*://*.doubleclick.net/*",
+    "*://partner.googleadservices.com/*",
+    "*://*.googlesyndication.com/*",
+    "*://*.google-analytics.com/*",
+    "*://creative.ak.fbcdn.net/*",
+    "*://*.adbrite.com/*",
+    "*://*.exponential.com/*",
+    "*://*.quantserve.com/*",
+    "*://*.scorecardresearch.com/*",
+    "*://*.zedo.com/*"
+  ];
+  
+  chrome.runtime.onInstalled.addListener(function() {
+    chrome.declarativeNetRequest.updateDynamicRules({
+      removeRuleIds: [1, 2, 3] // Replace with the actual rule IDs you want to remove
+    });
+  
+    const rules = defaultFilters.map((filter, index) => {
+      return {
+        id: index + 1, // Replace with a unique integer ID for each rule
+        priority: 1,
+        action: {
+          type: "block"
+        },
+        condition: {
+          urlFilter: filter
+        }
+      };
+    });
+  
+    chrome.declarativeNetRequest.updateDynamicRules({
+      addRules: rules
+    });
+  });
+  
